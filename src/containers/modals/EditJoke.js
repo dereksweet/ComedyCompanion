9081c3381d3351b502fac8d1bf7f7d4743275e80
Derek Sweet <derek@arizonabay.com>
2017-04-17 20:51:32 -0600

    Remove example model code

--- src/containers/modals/EditJoke.js
+++ src/containers/modals/EditJoke.js
@@ -11,29 +11,6 @@ import layoutStyles from '../../stylesheets/layoutStyles';
 export default class EditJoke extends Component {
   constructor(props) {
     super(props);
-
-    // let testJoke = new Joke();
-    // testJoke._in_development = true;
-    // testJoke._name = 'Test Name1';
-    // testJoke._notes = 'Test Notes1';
-    // testJoke._rating = 0.1;
-    // testJoke.save();
-
-    // Joke.get(1).then((joke) => {
-    //   console.log(joke);
-    //   joke._in_development = false;
-    //   joke.save();
-    // });
-
-    // Joke.all().then((jokes) => {
-    //   console.log(jokes);
-    // });
-
-    // Joke.where('AND', { _rating: "eq|0.2", _name: "eq|'Test Name2'" }).then((jokes) => {
-    //   console.log(jokes);
-    // });
-
-    // Joke.destroy_all();
   }
 
   render() {
