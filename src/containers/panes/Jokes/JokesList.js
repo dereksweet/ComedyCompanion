7bb9089ff8bdd4381012949986896cb04305717c
Derek Sweet <derek@arizonabay.com>
2017-04-20 23:04:45 -0600

    Clean up the JokeList page a bit

--- src/containers/panes/Jokes/JokesList.js
+++ src/containers/panes/Jokes/JokesList.js
@@ -6,11 +6,14 @@ import {bindActionCreators} from 'redux';
 import { connect } from 'react-redux';
 import {Button} from 'react-native-ui-xg';
 
+import Joke from '../../../models/Joke';
+
+import * as jokeActions from '../../../actions/jokeActions';
 import * as routingActions from '../../../actions/routingActions';
 
 import layoutStyles from '../../../stylesheets/layoutStyles';
 
-import {largeJokesIcon, addJokeIcon} from '../../../helpers/icons';
+import {largeJokesIcon, jokesIcon} from '../../../helpers/icons';
 
 class JokesList extends Component {
   constructor(props) {
@@ -18,15 +21,25 @@ class JokesList extends Component {
   }
 
   render() {
-    const { routingActions } = this.props;
+    const { jokeActions, routingActions } = this.props;
 
-    const addJoke = () => {
-      routingActions.openModal();
+    const editJoke = () => {
+      Joke.get(1).then((joke) => {
+        jokeActions.setJoke(joke);
+        routingActions.openModal();
+      });
     };
 
     return (
       <View style={layoutStyles.centeredFlex}>
-        <Text>Hi There</Text>
+        {largeJokesIcon}
+        <Text style={ {paddingTop: 25} }>You have a joke!</Text>
+        <Text style={ {paddingBottom: 20} }>Click the button below to edit it..</Text>
+        <View style={ {paddingBottom: 100} }>
+          <Button type="surface" size="large" theme="red" onPress={ editJoke }>
+            <Text style={{ color: '#FFFFFF' }}>Edit Joke</Text>
+          </Button>
+        </View>
       </View>
     );
   }
@@ -36,6 +49,7 @@ export default connect(state => ({
 
   }),
   (dispatch) => ({
+    jokeActions: bindActionCreators(jokeActions, dispatch),
     routingActions: bindActionCreators(routingActions, dispatch)
   })
 )(JokesList);
