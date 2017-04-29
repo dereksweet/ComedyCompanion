6320ea7908dc91a7d0e78a1b9ce3406497b5ec93
Derek Sweet <derek@arizonabay.com>
2017-04-29 09:18:29 -0600

    Allow edit set list page to respond to appearance of keyboard.

--- src/containers/modals/EditSetList.js
+++ src/containers/modals/EditSetList.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, {Component} from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight, Platform, Keyboard } from 'react-native';
 import {bindActionCreators} from 'redux';
 import { connect } from 'react-redux';
 import {Button} from 'react-native-ui-xg';
@@ -9,15 +9,51 @@ import {Button} from 'react-native-ui-xg';
 import JokeSelector from './EditSetList/JokeSelector';
 
 import * as routingActions from '../../actions/routingActions';
-import * as jokeListActions from '../../actions/jokeListActions';
-
-import JokeListHelper from '../../helpers/jokeListHelper';
 
 import layoutStyles from '../../stylesheets/layoutStyles';
 
 class EditSetList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      modal_height: 0,
+      keyboard_height: 0
+    }
+  }
+
+  componentWillMount () {
+    var eventVerb = Platform.OS === 'ios'? 'Will' : 'Did';
+
+    this.keyboardDidShowListener = Keyboard.addListener('keyboard' + eventVerb + 'Show', this.keyboardDidShow.bind(this));
+    this.keyboardDidHideListener = Keyboard.addListener('keyboard' + eventVerb + 'Hide', this.keyboardDidHide.bind(this));
+  }
+
+  keyboardDidShow (e) {
+    this.setState({
+      keyboard_height: e.endCoordinates.height
+    });
+  }
+
+  keyboardDidHide (e) {
+    this.setState({
+      keyboard_height: 0
+    });
+  }
+
+  componentWillUnmount () {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+
+  measureModalView(event) {
+    this.setState({
+      modal_height: event.nativeEvent.layout.height
+    });
+  }
+
+  contentHeight() {
+    return this.state.modal_height - (Platform.OS === 'ios'? this.state.keyboard_height : 0);
   }
 
   render() {
@@ -30,25 +66,27 @@ class EditSetList extends Component {
     return (
       <View style={layoutStyles.centeredFlex}>
         <View style={layoutStyles.statusBarBuffer} />
-        <View style={layoutStyles.modalContent}>
-          <View style={{ flex: 1, flexDirection: 'row', borderTopColor: '#CCCCCC', borderTopWidth: 1 }}>
-            <View style={{ flex: 1, borderRightColor: '#CCCCCC', borderRightWidth: 1 }}>
-              <JokeSelector />
-            </View>
-            <View style={{ flex: 1 }}>
+        <View style={layoutStyles.modalContent} onLayout={(event) => this.measureModalView(event)}>
+          <View style={{ height: this.contentHeight() }}>
+            <View style={{ flex: 1, flexDirection: 'row', borderTopColor: '#CCCCCC', borderTopWidth: 1 }}>
+              <View style={{ flex: 1, borderRightColor: '#CCCCCC', borderRightWidth: 1 }}>
+                <JokeSelector />
+              </View>
+              <View style={{ flex: 1 }}>
 
+              </View>
             </View>
-          </View>
-          <View style={ { flexDirection: 'row', width: '100%' }}>
-            <View style={ { flex: 1 } }>
-              <Button type="surface" size="large" theme="gray" onPress={ cancel }>
-                <Text style={layoutStyles.buttonText}>Cancel</Text>
-              </Button>
-            </View>
-            <View style={ { flex: 1 } }>
-              <Button type="surface" size="large" theme="blue" onPress={ cancel }>
-                <Text style={layoutStyles.buttonText}>Save</Text>
-              </Button>
+            <View style={ { flexDirection: 'row', width: '100%' }}>
+              <View style={ { flex: 1 } }>
+                <Button type="surface" size="large" theme="gray" onPress={ cancel }>
+                  <Text style={layoutStyles.buttonText}>Cancel</Text>
+                </Button>
+              </View>
+              <View style={ { flex: 1 } }>
+                <Button type="surface" size="large" theme="blue" onPress={ cancel }>
+                  <Text style={layoutStyles.buttonText}>Save</Text>
+                </Button>
+              </View>
             </View>
           </View>
         </View>
@@ -58,10 +96,9 @@ class EditSetList extends Component {
 }
 
 export default connect(state => ({
-    jokeListState: state.joke_list
+
   }),
   (dispatch) => ({
-    routingActions: bindActionCreators(routingActions, dispatch),
-    jokeListActions: bindActionCreators(jokeListActions, dispatch)
+    routingActions: bindActionCreators(routingActions, dispatch)
   })
 )(EditSetList);
