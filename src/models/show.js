d36047eb6ce2d0b7956a75524f549702ba6ddc0d
Derek Sweet <derek@arizonabay.com>
2017-05-03 22:32:00 -0600

    Add Show model

---
+++ src/models/show.js
@@ -0,0 +1,39 @@
+import Model from './Model';
+import SetList from './set_list';
+
+export default class Show extends Model {
+  static databaseName() {
+    return "CCDB";
+  }
+
+  static tableName() {
+    return "shows";
+  }
+
+  static dateFields() {
+    return ['_date', '_created_at', '_updated_at'];
+  }
+
+  constructor(data = {
+    _id: -1,
+    _venue: '',
+    _date: new Date(),
+    _city: '',
+    _state: '',
+    _set_list: new SetList(),
+    _created_at: new Date(),
+    _updated_at: new Date()
+  })
+  {
+    super();
+
+    this._id             = data._id;
+    this._venue          = data._venue;
+    this._date           = data._date;
+    this._city           = data._city;
+    this._state          = data._state;
+    this._set_list       = new SetList(data._set_list);
+    this._created_at     = data._created_at;
+    this._updated_at     = data._updated_at;
+  }
+}
