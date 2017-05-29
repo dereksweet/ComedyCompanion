ad2b68fed8b7f0e9d3874393f7523f8135d527f6
Derek Sweet <derek@arizonabay.com>
2017-05-29 07:30:17 -0600

    Allow searching by city as well as venue for shows

--- src/helpers/showListHelper.js
+++ src/helpers/showListHelper.js
@@ -17,8 +17,8 @@ export default class ShowListHelper extends Component {
     const my_sort_order = sort_order || showListState.sort_order;
 
     Show.where(
-      { '_venue': "LIKE|'" + my_venue_filter + "'" },
-      'AND',
+      { '_venue': "LIKE|'" + my_venue_filter + "'", '_city': "LIKE|'" + my_venue_filter + "'" },
+      'OR',
       my_sort_field,
       my_sort_order
     ).then((shows) => {
