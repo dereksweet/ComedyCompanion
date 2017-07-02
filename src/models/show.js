31d8db7781faeaad6b064f8743f1fec3fe9780db
Derek Sweet <derek@arizonabay.com>
2017-07-02 08:57:34 -0600

    Convert all the existing models to defaults based structure

--- src/models/show.js
+++ src/models/show.js
@@ -3,6 +3,15 @@ import SetList from './set_list';
 
 import uuidV4 from 'uuid/v4';
 
+const defaults = {
+  _uuid: uuidV4(),
+  _venue: '',
+  _date: new Date(),
+  _city: '',
+  _state: '',
+  _set_list: new SetList()
+};
+
 export default class Show extends Model {
   static databaseName() {
     return "CCDB";
@@ -22,12 +31,12 @@ export default class Show extends Model {
 
   constructor(data = {
     _id: -1,
-    _uuid: uuidV4(),
-    _venue: '',
-    _date: new Date(),
-    _city: '',
-    _state: '',
-    _set_list: new SetList(),
+    _uuid: defaults._uuid,
+    _venue: defaults._venue,
+    _date: defaults._date,
+    _city: defaults._city,
+    _state: defaults._state,
+    _set_list: defaults._set_list,
     _created_at: new Date(),
     _updated_at: new Date()
   })
@@ -35,12 +44,12 @@ export default class Show extends Model {
     super();
 
     this._id             = data._id;
-    this._uuid           = data._uuid;
-    this._venue          = data._venue;
-    this._date           = data._date;
-    this._city           = data._city;
-    this._state          = data._state;
-    this._set_list       = new SetList(data._set_list);
+    this._uuid           = data._uuid || defaults._uuid;
+    this._venue          = data._venue || defaults._venue;
+    this._date           = data._date || defaults._date;
+    this._city           = data._city || defaults._city;
+    this._state          = data._state || defaults._state;
+    this._set_list       = data.set_list ? new SetList(data._set_list) : defaults._set_list;
     this._created_at     = data._created_at;
     this._updated_at     = data._updated_at;
   }
