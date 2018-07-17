7704beaa0bbda0c712507c8ecedae533220f76b0
Derek Sweet <derek@arizonabay.com>
2018-07-16 23:53:16 -0600

    cleanup reducers

--- src/reducers/joke_list.js
+++ src/reducers/joke_list.js
@@ -19,55 +19,46 @@ export default function joke_list(state = initialState, action = {}) {
         ...state,
         joke_list: action.joke_list
       };
-      break;
     case types.SET_JOKE_LIST_SELECTOR:
       return {
         ...state,
         joke_list_selector: action.joke_list
       };
-      break;
     case types.SET_JOKE_LIST_EMPTY:
       return {
         ...state,
         empty: action.empty
       };
-      break;
     case types.SET_JOKE_LIST_SORT_FIELD:
       return {
         ...state,
         sort_field: action.sort_field
       };
-      break;
     case types.SET_JOKE_LIST_SORT_ORDER:
       return {
         ...state,
         sort_order: action.sort_order
       };
-      break;
     case types.SET_JOKE_LIST_FILTER:
       return {
         ...state,
         name_filter: action.name_filter
       };
-      break;
     case types.SET_JOKE_LIST_FILTER_SELECTOR:
       return {
         ...state,
         name_filter_selector: action.name_filter
       };
-      break;
     case types.TOGGLE_JOKE_LIST_IN_DEVELOPMENT:
       return {
         ...state,
         in_development: !state.in_development
       };
-      break;
     case types.TOGGLE_JOKE_LIST_IN_DEVELOPMENT_SELECTOR:
       return {
         ...state,
         in_development_selector: !state.in_development_selector
       };
-      break;
     default:
       return state;
   }
--- src/reducers/show_list.js
+++ src/reducers/show_list.js
@@ -15,31 +15,26 @@ export default function show_list(state = initialState, action = {}) {
         ...state,
         show_list: action.show_list
       };
-      break;
     case types.SET_SHOW_LIST_EMPTY:
       return {
         ...state,
         empty: action.empty
       };
-      break;
     case types.SET_SHOW_LIST_SORT_FIELD:
       return {
         ...state,
         sort_field: action.sort_field
       };
-      break;
     case types.SET_SHOW_LIST_SORT_ORDER:
       return {
         ...state,
         sort_order: action.sort_order
       };
-      break;
     case types.SET_SHOW_LIST_FILTER:
       return {
         ...state,
         venue_filter: action.venue_filter
       };
-      break;
     default:
       return state;
   }
